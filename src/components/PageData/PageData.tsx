de9281f8c414897af220b83483146f7fd3cfba01
benj <benj.ledesma@gmail.com>
2020-04-24 02:20:49 +0800

    added fb page data sync and save (about, etc)

--- src/components/PageData/PageData.tsx
+++ src/components/PageData/PageData.tsx
@@ -1,12 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import firebase from './../Firebase';
 
 // @ts-ignore
 import FB from 'fb';
-
-const appCredentials = require('./../../credentials.json');
-const facebookCredentials = appCredentials.facebook;
 
 // firebase init
 let db = firebase.firestore();
@@ -14,12 +10,10 @@ var provider = new firebase.auth.FacebookAuthProvider();
 provider.addScope('email');
 provider.addScope('manage_pages');
 
-const fbGraph = "https://graph.facebook.com/v6.0/";
-
 
 function PageData() {
   const [ currentUser, setCurrentUser ] = useState<string>();
-  const [ fbPages, setFbPages ] = useState([{}]);
+  const [ fbPages, setFbPages ] = useState<Array<any>>();
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user: any) => {
@@ -28,17 +22,60 @@ function PageData() {
     });
   });
 
-  const getPageData = (): void => {
-    return
+  const getFacebookPages = async () => {
+    console.log('getting page data from firebase');
+    var userDoc = db.collection('users').doc(currentUser);
+    const fbPagesFirebase = await userDoc.collection('pages').get();
+    setFbPages(fbPagesFirebase.docs.map((doc: any) => doc.data()));
   }
+
+  const syncPageData = async () => {
+    // get user's fb pages
+    var userDoc = db.collection('users').doc(currentUser);
+    const fbPages = await userDoc.collection('pages').get();
+    console.log('getting data from facebook');
+    for (let pageDoc of fbPages.docs) {
+      const { access_token, id } = pageDoc.data();
+      const requestFields = {
+        fields: 'about,bio,location,website'
+      };
+
+      FB.setAccessToken(access_token);
+      FB.api(id, requestFields).then((result: any) => {
+        // add fb page details to pages doc
+        userDoc.collection('pages').doc(id).update(result);
+      }).catch((err: Error) => {
+        console.error(err);
+      })
+    }
+  }
+
+  useEffect(() => {
+    if (currentUser && typeof fbPages === 'undefined') {
+      getFacebookPages();
+    }
+  });
+
+  const facebookPages = fbPages?.map((pageData: any) => {
+    let { id, about, category, name, website } = pageData;
+    return (
+      <div key={id} className="FbPageData">
+        <h3>{ name }</h3>
+        <b>{ category }</b>
+        <p>{ about }</p>
+        { (!website) ? null : <p>{ website }</p> }
+      </div>
+    )
+  })
 
 
 
   return (
     <div className="AuthContainer">
       <h1>Page Data</h1>
-      <button onClick={getPageData}>Get Page Data</button>
+      <button onClick={syncPageData}>Sync Page Data</button>
       <div className="FbPageDetails">
+        { facebookPages }
       </div>
     </div>
   )
