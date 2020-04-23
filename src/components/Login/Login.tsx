a846dfbddcda0c4686aacd88bc2ddf44ae83347b
benj <benj.ledesma@gmail.com>
2020-04-24 01:46:25 +0800

    made functions async, some typing improvements

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -19,7 +19,7 @@ const fbGraph = "https://graph.facebook.com/v6.0/";
 
 function Login() {
   const [ currentUser, setCurrentUser ] = useState<string>();
-  const [ fbPages, setFbPages ] = useState([{}]);
+  const [ fbPages, setFbPages ] = useState<Array<any>>();
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user: any) => {
@@ -30,84 +30,83 @@ function Login() {
 
 
   const facebookSignUp = (): void => {
-    firebase.auth().signInWithPopup(provider).then((result: any) => {
+    firebase.auth().signInWithPopup(provider).then(async (result: any) => {
       // user gets saved to firebase auth already (result.user)
       const uid: string = result.user.uid;
       const tempAccessToken: string = result.credential!.accessToken;
       
       // get long-lived user access token
       console.log('getting FB long-lived access token')
-      axios.get(fbGraph + decodeURI(`
-        oauth/access_token
-          ?grant_type=fb_exchange_token&
-          client_id=${facebookCredentials.clientId}&
-          client_secret=${facebookCredentials.clientSecret}&
-          fb_exchange_token=${tempAccessToken}
-        `).replace(/ /g, "")
-        ).then((res: any) => {
-          const accessToken = res.data.access_token;
+      const fbGraphRes: any = await axios.get(
+        fbGraph + decodeURI(`
+          oauth/access_token
+            ?grant_type=fb_exchange_token&
+            client_id=${facebookCredentials.clientId}&
+            client_secret=${facebookCredentials.clientSecret}&
+            fb_exchange_token=${tempAccessToken}
+          `).replace(/ /g, "")
+      );
+      const accessToken = fbGraphRes.data.access_token;
           
-          // get facebook userid using old access token
-          FB.setAccessToken(tempAccessToken);
-          console.log('getting FB userid')
-          FB.api('me', (res: any) => {
-            const userId: string = res.id;
+      // get facebook userid using old access token
+      FB.setAccessToken(tempAccessToken);
+      console.log('getting FB userid')
+      const fbUserRes: any = await FB.api('me');
+      const userId: string = fbUserRes.id;
             
-            // save firestore creds based on facebook login
-            console.log('saving to Firestore');
-            let ref = db.collection('users').doc(uid);
-            let setAuth = ref.update({
-              auth: {
-                facebook: { userId, accessToken }
-              }
-            });
-            setAuth.catch((err) => {
-              console.error(err);
-            });
-          });
-        })
-    }).catch(function(error) {
+      // save firestore creds based on facebook login
+      console.log('saving to Firestore');
+      let ref = db.collection('users').doc(uid);
+      await ref.set({
+        auth: {
+          facebook: { userId, accessToken }
+        }
+      });
+    }).catch((error: any) => {
       console.error(error);
     });
   }
 
-  const listFacebookPages = (): void => {
+  const listFacebookPages = async () => {
     var userDoc = db.collection('users').doc(currentUser);
-    userDoc.get().then((doc: any) => {
-      if (!doc.exists) {
-        console.log('user not authenticated for Facebook!');
-        return
-      }
+    const doc = await userDoc.get();
+    if (!doc.exists) {
+      console.log('user not logged in!');
+      return
+    }
       
-      let firestorePages = doc.data().pages;
-      if (firestorePages) {
-        console.log('getting data from Firestore');
-        setFbPages(firestorePages);
-        return
-      }
+    let userPagesRef = userDoc.collection('pages');
+    const pageDocs = await userPagesRef.get();
+    if (pageDocs.docs.length > 0) {
+      console.log('getting data from firestore')
+      const firestorePages: Array<any> = pageDocs.docs.map((doc) => doc.data());
+      setFbPages(firestorePages);
+      return
+    }
 
-      // user long-lived access token to get allowed pages (from signup)
-      FB.setAccessToken(doc.data().facebook.accessToken);
-      console.log('getting data from Facebook');
-      FB.api('me/accounts').then((result: any) => {
-        const pages: Array<Object> = result.data;
-        userDoc.update({ pages })
-          .catch(err => {
-            console.error(err);
-          });
-        setFbPages(pages);
-      })
-    })
+    // user long-lived access token to get allowed pages (from signup)
+    FB.setAccessToken(doc!.data()!.auth.facebook.accessToken);
+    console.log('getting data from Facebook');
+    const result: any = await FB.api('me/accounts');
+    const fbPages: Array<any> = result.data;
+    setFbPages(fbPages);
+    for(let page of fbPages) {
+      userDoc.collection('pages')
+        .doc(page.id).set(page)
+        .catch(err => {
+          console.error(err)
+        })
+    }
   }
 
   useEffect(() => {
-    if (currentUser && Object.keys(fbPages[0]).length === 0) {
+    if (currentUser && typeof fbPages === 'undefined') {
       listFacebookPages();
     }
   });
 
-  const pageElements = fbPages.map((pageData: any) => (
-    (pageData.name) ? <li key={pageData.id}>{ pageData.name }</li> : null
+  const pageElements = fbPages?.map((pageData: any) => (
+    <li key={pageData.id}>{ pageData.name }</li>
   ))
 
   return (
